ca6175ebb865fdbe28dd8862ebaa992fe3590cf7
Paul Codding <pcodding@gmail.com>
2012-08-09 08:08:36 -0700

    Updated script with better visual delineation between checks and outputs

--- Installation/pre_install_check.sh
+++ Installation/pre_install_check.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
-# 
-#  pre_install_check.sh
-#  Hortonworks SE Tools
-#  
-#  Created by Paul Codding on 2012-08-08.
-#  
-#  This script is used to run a quick check before building out a cluster to allow the user to check to ensure all pre-reqs have been met
-#
-
 SSHKEY=/tmp/ec2-keypair.pem
+TPUT='tput -T xterm-color'
+txtund=$(${TPUT} sgr 0 1)          # Underline
+txtbld=$(${TPUT} bold)             # Bold
+txtrst=$(${TPUT} sgr0)             # Reset
+
 for host in `cat Hostdetail.txt`; do
-  echo -e "\n######################################################"
+  echo -e "${txtbld}\n######################################################"
   echo -e "# Checking Host: $host"
-  echo "######################################################"
-  ssh -i $SSHKEY root@$host PREREQS=$PREREQS 'bash -s' << 'END'
-  PREREQS=( yum rpm ssh curl wget net-snmp net-snmp-utils )
-  POSSIBLE_CONFLICTS=( ruby puppet nagios mysql ganglia passenger )
-  echo -e "[*] Checking base system requirements for host $host"
-  echo -e "\n[**] Checking Processors"
+  echo "######################################################${txtrst}"
+  ssh -i $SSHKEY root@$host 'bash -s' << 'END'
+  function printHeading() {
+    echo -e "\n${txtund}[*] $1 \n######################################################${txtrst}"
+  }
+  PREREQS=( yum rpm ssh curl wget net-snmp net-snmp-utils ntpd )
+  POSSIBLE_CONFLICTS=( ruby puppet nagios mysql ganglia passenger cloudera hadoop )
+  printHeading "Checking Processors"
   cat /proc/cpuinfo  | grep 'model name' | awk -F': ' '{ print $2; }'
-  echo -e "\n[**] Checking RAM"
+  printHeading "Checking RAM"
   cat /proc/meminfo  | grep MemTotal | awk '{ print $2/1024/1024 " GB"; }'
-  echo -e "\n[**] Checking disk space"
+  printHeading "Checking disk space"
   df -h
-  echo -e "\n[**] Checking OS and arch"
+  printHeading "Checking OS and arch"
   cat /etc/issue.net | head -1 
   arch
-  echo -e "\n[**] Checking host files"
+  printHeading "Checking host files"
   cat /etc/hosts
-  echo -e "\n[**] Checking iptables"
+  printHeading "Checking hostname"
+  echo "hostname -f: `hostname -f`" 
+  echo "hostname -i: `hostname -i`"
+  printHeading "Checking iptables"
   iptables -vnL
-  echo -e "\n[**] Checking SELinux configuration"
+  printHeading "Checking SELinux configuration"
   cat /etc/selinux/config | grep ^SELINUX
-  echo -e "\n[**] Checking prereq packages"
+  printHeading "Checking prereq packages"
   RPMS=`rpm -qa`
   for package in ${PREREQS[@]}; do
     echo -n "Looking for: $package - "

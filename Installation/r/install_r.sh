41a908f6d13c78e2ac2d6fadcbd1d6c7fe5949df
Paul Codding <pcodding@gmail.com>
2012-10-15 18:22:09 -0700

    Added Apache Licensing.

--- Installation/r/install_r.sh
+++ Installation/r/install_r.sh
@@ -3,6 +3,22 @@
 # Author: Paul Codding <paul@hortonworks.com>
 # IMPORTANT: Please make sure you update the path to the .tar.gz files 
 # referenced below to the actual path on your cluster!
+# 
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 export RMR_PATH=/root/hdp/r/rmr_1.3.1.tar.gz
 export RHDFS_PATH=/root/hdp/r/rhdfs_1.0.5.tar.gz
 export RHBASE_PATH=/root/hdp/r/rhbase_1.0.4.tar.gz
